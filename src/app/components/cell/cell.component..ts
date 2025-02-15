b9c8d97435106ebb346c961dc7e920c44e4900db
the-wind-dev <80009607+the-wind-dev@users.noreply.github.com>
2025-02-15 14:27:30 +0300

    cell component

--- src/app/components/cell/cell.component..ts
+++ src/app/components/cell/cell.component..ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Cell } from '../../models';
 
 @Component({
   selector: 'app-cell',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './cell.component.html',
   styleUrls: ['./cell.component.scss']
 })
-export class CellComponent {}
+export class CellComponent {
+  @Input() cell!: Cell;
+
+  @Output() onReveal = new EventEmitter<{ x: number; y: number }>();
+  @Output() onFlag = new EventEmitter<{ x: number; y: number, isFlagged: boolean }>();
+
+  handleLeftClick(): void {
+    const {isOpened, isFlagged, x, y} = this.cell;
+
+    if (!isOpened && !isFlagged) {
+      this.onReveal.emit({x, y});
+    }
+  }
+
+  handleRightClick(): void {
+    const {isOpened, isFlagged, x, y} = this.cell;
+    if (!isOpened) {
+      this.onFlag.emit({x, y, isFlagged});
+    }
+  }
+}
